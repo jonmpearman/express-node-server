c27ed8021ca2df8df135f8321ffec5d1cdb4bb45
Jon Pearman <jpearman@sbgtv.com>
2019-09-16 09:37:10 -0700

    removing some left over code from the hack a thon

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/send', (req, res) => {
-    res.sendFile(path.join(__dirname + '/template.html'));
-})
-
-app.use('/send-notification', (req, res) => {
-    console.log('LogMe what is the req data? .... ', req);
-    res.redirect('/send');
-})
-
 app.get('/', (req, res) => {
     res.send('It Worked!');
 });
